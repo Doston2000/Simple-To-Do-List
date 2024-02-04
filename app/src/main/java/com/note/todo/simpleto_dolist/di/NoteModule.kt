6a4bf4518420252f4e5610971c3eb9307826d9d3
DOSTON <dostondavronov67@gmail.com>
2024-02-04 20:26:56 +0500

    AddEditNoteViewModel+

--- app/src/main/java/com/note/todo/simpleto_dolist/di/NoteModule.kt
+++ app/src/main/java/com/note/todo/simpleto_dolist/di/NoteModule.kt
@@ -7,6 +7,7 @@ import com.note.todo.simpleto_dolist.future_note.data.repository.NoteRepositoryI
 import com.note.todo.simpleto_dolist.future_note.domain.repository.NoteRepository
 import com.note.todo.simpleto_dolist.future_note.domain.use_case.AddNote
 import com.note.todo.simpleto_dolist.future_note.domain.use_case.DeleteNote
+import com.note.todo.simpleto_dolist.future_note.domain.use_case.GetNote
 import com.note.todo.simpleto_dolist.future_note.domain.use_case.GetNotes
 import com.note.todo.simpleto_dolist.future_note.domain.use_case.NoteUseCases
 import dagger.Module
@@ -43,7 +44,8 @@ object NoteModule {
         return NoteUseCases(
             getNotes = GetNotes(repository),
             deleteNote = DeleteNote(repository),
-            addNote = AddNote(repository)
+            addNote = AddNote(repository),
+            getNote = GetNote(repository)
         )
     }
 
